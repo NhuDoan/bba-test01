7abe16bc31b9cea3d14f03180f4e0676bb5fd2ad
test 01 - NhuDoan <doannhubl@gmail.com>
2026-01-29 01:02:43 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,7 @@
+const playerName = "Mario";
+let currentLives = 3;
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+const averageCoin = (level1 + level2 + level3) / 3;
+console.log('Giá trị trung bình coin của 3 level là: ',averageCoin);
